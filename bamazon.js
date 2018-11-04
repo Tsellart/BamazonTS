b6ad75a74f230a59cf30da29396f3b5a03388d88
unknown <whigs93@gmail.com>
2018-11-03 23:25:27 -0500

    stuff

--- bamazon.js
+++ bamazon.js
@@ -1,6 +1,7 @@
 var mysql = require("myswl");
+var inquirer = require("inquirer");
 
-var conenction = mysql.createConnection({
+var connection = mysql.createConnection({
     host: "localhost" ,
 
     port: 3306,
@@ -11,8 +12,64 @@ var conenction = mysql.createConnection({
     database: "bamazon_db"
 });
 
+conenction.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+})
+
 conenction.connect(function(err){
     if (err) throw err;
-    console.log("connected as id " + conenction,threadId + "\n");
-    createProduct
-})
+    console.log("connected as id " + connection.threadId + "\n");
+    startPurchase();
+
+});
+
+function startPurchase() {
+    inquirer.prompt([
+        {
+            name: "id",
+            type: "input",
+            message: "What is the item you would like to buys ID?",
+            validate: validateInput,
+            filter: Number
+
+        },
+        {
+            name: "stock_quantity",
+            type: "input",
+            message: "How many would you like?",
+            validate: validateInput,
+            filter:Number
+        }
+        ]).then(function(answer) {
+
+            var item = input.id;
+            var quantity = input.stock_quantity;
+
+            connection.query(
+                "SELECT * FROM products WHERE ?",
+                {
+                    id: item
+                },
+
+                function(err) {
+                    if (err) throw err;
+                    if (data.length === 0) {
+                        console.log('ERROR: Invalid Item ID, please select a valid ID');
+                        
+                    } else {
+                        var productData = data[0];
+
+                        if (quantity <= productData.stock_quantity) {
+                            console.log('Congrats! Your Item is ordered!');
+
+                            connection.query("UPDATE products SET stock_quantity" + productData.stock_quantity - quantity + "WHERE id" + item);
+                            console.log(this);
+                            connection.end();
+                        }
+                    }
+                } 
+
+            )
+        })
+}
